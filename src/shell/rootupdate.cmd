d55d67f9e73ad94865ad7e8f83677a8aaaa8f598
yves40 <yves.toubhans@free.fr>
2021-06-27 18:41:29 +0200

    Not used

--- src/shell/rootupdate.cmd
+++
@@ -1,9 +0,0 @@
-rem ----------------------------------------------------------------------------------------
-rem       hostc.cmd
-rem 
-rem 	  Jun 27 2021   Initial
-rem ----------------------------------------------------------------------------------------
-scp2 -a D:\ALL\LAB\GIT\cams2021\src\shell\motd  root@vboxnode:/etc
-scp2 -a D:\ALL\LAB\GIT\cams2021\src\shell\motd  root@zerasp:/etc
-
-
