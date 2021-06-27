685f944972e12e87908282fe2a65ca25976f7a92
yves40 <yves.toubhans@free.fr>
2021-06-27 16:32:35 +0200

    WIP on cams2021 revival

---
+++ src/shell/rootupdate.cmd
@@ -0,0 +1,9 @@
+rem ----------------------------------------------------------------------------------------
+rem       hostc.cmd
+rem 
+rem 	  Jun 27 2021   Initial
+rem ----------------------------------------------------------------------------------------
+scp2 -a D:\ALL\LAB\GIT\cams2021\src\shell\motd  root@vboxnode:/etc
+scp2 -a D:\ALL\LAB\GIT\cams2021\src\shell\motd  root@zerasp:/etc
+
+
