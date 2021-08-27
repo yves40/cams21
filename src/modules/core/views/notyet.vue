2c92af5fda821db13fc03cb0ba9336353cd55a67
yves40 <yves.toubhans@free.fr>
2021-08-27 10:38:32 +0200

    Fix old bootstrap code problem

--- src/modules/core/views/notyet.vue
+++ src/modules/core/views/notyet.vue
@@ -5,35 +5,12 @@
   Jan 02 2020   Initial
 -->
 <template>
-  <div>
-      <b-container>
-        <b-row>
-          <b-col cols="2"></b-col>
-          <b-col>
-            <img src="../../../images/WIP.png" />
-          </b-col>
-          <b-col cols="2"></b-col>
-        </b-row>
-        <b-row>
-          <b-col cols="2"></b-col>
-          <b-col>
-            <b-list-group >
-              <b-list-group-item>{{version}}</b-list-group-item>
-            </b-list-group>
-          </b-col>
-          <b-col cols="2"></b-col>
-        </b-row>
-        <b-row>
-          <b-col cols="2"></b-col>
-          <b-col>
-            <p></p> 
-            <p> This feature is not yet implemented.</p>
-            <p></p> 
-            <p>We're working hard to bring it online asap</p> 
-          </b-col>
-          <b-col cols="2"></b-col>
-        </b-row>
-      </b-container>
+  <div class="centeredtext">
+      <h4>Cams utility</h4>
+      <img src="../../../images/WIP.png" />
+      <p> This feature is not yet implemented.</p>
+      <p></p> 
+      <p>We're working hard to bring it online asap</p> 
   </div>
 </template>
 
@@ -43,7 +20,7 @@
 export default {
   data() {
     return {
-      version: "Notyet 1.00, Jan 02 2020"
+      version: "Notyet 1.01, Aug 27 2021"
     };
   },
 };
