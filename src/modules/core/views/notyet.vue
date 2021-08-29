dff5025e2b99e3458923f43e5b7773ae7d85ea29
yves40 <yves.toubhans@free.fr>
2021-08-29 11:48:21 +0200

    Cleanup some css

--- src/modules/core/views/notyet.vue
+++ src/modules/core/views/notyet.vue
@@ -5,6 +5,7 @@
   Jan 02 2020   Initial
   Aug 27 2021   cams2021
   Aug 28 2021   Add optional buttons
+  Aug 29 2021   Small adjustments
   
 -->
 <template>
@@ -12,10 +13,11 @@
       <h4>{{version}}</h4>
       <img src="../../../images/WIP.png" height="150" width="150"/>
       <p> This feature is not yet implemented.</p>
-      <p></p> 
       <p>We're working hard to bring it online asap</p>
-      <p class="underlined">Coming from menu entry:[{{props.from}}] : {{props.message}}</p>
-      <p></p>
+
+        <p class="underlined border">{{props.message}}</p>
+
+
       <button class="button" v-show="okbutton" @click="click('OK')">{{props.ok}}</button>
       <button class="button red" v-show="cancelbutton"  @click="click('CANCEL')">{{props.cancel}}</button>
       <button class="button blue" v-show="backbutton"  @click="click('BACK')">{{props.back}}</button>
@@ -42,7 +44,7 @@ export default {
   },
   setup(props) {
 
-    const version = "Notyet 1.29, Aug 28 2021 ";
+    const version = "Notyet 1.30, Aug 29 2021 ";
     const router = useRouter();
     let okbutton = false;
     let cancelbutton = false;
