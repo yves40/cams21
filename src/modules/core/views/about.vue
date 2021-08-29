bec16fc7473caf761d4510147d0c1eb9883c1386
yves40 <yves.toubhans@free.fr>
2021-08-29 19:31:21 +0200

    Add a button to point home

--- src/modules/core/views/about.vue
+++ src/modules/core/views/about.vue
@@ -1,14 +1,23 @@
 <template>
     <div class="centeredtext">
-      <h4>Cams utility</h4>
+      <h4>{{version}}</h4>
       <p class="underlined">This is the amazing cams utility application Currently supports Heden and Foscam devices</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nisi atque sunt, eveniet odio laboriosam ratione ipsa, praesentium voluptates magnam quisquam nam voluptatum doloremque dicta? Maxime voluptatum pariatur recusandae blanditiis veritatis voluptates nemo facilis accusamus. Tempora quibusdam nostrum libero amet.</p>
-  </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nisi atque sunt, eveniet odio laboriosam ratione ipsa, praesentium voluptates magnam quisquam nam voluptatum doloremque dicta? Maxime voluptatum pariatur recusandae blanditiis veritatis voluptates nemo facilis accusamus. Tempora quibusdam nostrum libero amet.</p> 
+      <router-link :to="{ name: 'home'}"><button class="button">Home</button></router-link>
+    </div>  
 </template>
 
 <script>
 
   export default {
     name: 'about',
+
+    setup() {
+      const version = "about 1.07, Aug 29 2021"
+
+      return {
+        version,
+      }
+    }
   }
 </script>
