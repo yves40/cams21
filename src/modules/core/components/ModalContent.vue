4fef9217451db8adb160d4275f0f8ca0dbe5f5af
yves40 <yves.toubhans@free.fr>
2021-09-04 18:32:06 +0200

    Look @ transition

--- src/modules/core/components/ModalContent.vue
+++ src/modules/core/components/ModalContent.vue
@@ -1,10 +1,12 @@
 
 <template>
-  <div class="look">
-    <h2>{{title}}</h2>
-    <p>{{msg}}</p>
-    <button class="button" @click="$emit('action')">{{label}}</button>
-  </div>
+  <transition name="pop" appear>
+    <div class="look">
+      <h2>{{title}}</h2>
+      <p>{{msg}}</p>
+      <button class="button" @click="$emit('action')">{{label}}</button>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -26,6 +28,12 @@ export default {
 }
 </script>
 <style scoped>
+.pop-enter-from { opacity: 0 }
+.pop-enter-to { opacity: 1 }
+.pop-enter-active, .pop-leave-active { transition: all 2s ease };
+.pop-leave-from { opacity: 1 }
+.pop-leave-to { opacity: 0 }
+
 .look {
   display: flex;
   flex-direction: column;
