1a71625bc88cc99a7b1d3c4d95a6fde5279229e0
yves40 <yves.toubhans@free.fr>
2021-09-07 19:29:52 +0200

    Finalize....

--- src/modules/core/views/notfound.vue
+++ src/modules/core/views/notfound.vue
@@ -1,6 +1,10 @@
 <template>
 <div class="centeredtext">
   <h1>Oops, it looks like the page you're looking for doesn't exist.</h1>
+  <h3>This can be either a bug or you typed a wrong URL</h3>
+  <p>In case the problem persists send a email to one of these contacts here.
+    <button class="button red" @click="click('CONTACTS')">Contacts</button>
+  </p>
   <p>{{version}}</p>
   <button class="button red" @click="click('HOME')">Home</button>
 </div>
@@ -16,11 +20,13 @@ import { useRouter } from 'vue-router'
 export default {
   setup(props) {
 
-    const version = "notfound 1.01, Sep 07 2021 ";
+    const version = "notfound 1.02, Sep 07 2021 ";
     const router = useRouter();
     function click(action) {
       switch(action) {
         case 'HOME': router.push('home');
+              break;
+        case 'CONTACTS': router.push({name: 'contact', params: { ok: 'Home', okr: 'home'}});
               break;
       }
     }
