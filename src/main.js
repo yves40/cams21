b73b20fa5e82d7f4a79fb5ce497615b8018293ce
yves40 <yves.toubhans@free.fr>
2021-09-09 16:05:29 +0200

    AppLink component

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 /*----------------------------------------------------------------------------
     Jul 18 2021   Initial
+    Sep 09 2021   AppLink component ( used to route vue or html requests)
 ----------------------------------------------------------------------------*/
 import { createApp } from 'vue'
 import App from './App.vue'
 
 import routes from './vue-router/allroutes'
 import store from './store/allstore'
+import AppLink from './modules/core/components/AppLink'
 
 //import './css/thecss.css'
 import './css/main.css'
 
 const app = createApp(App);
+
+
+app.component('AppLink', AppLink);
 app.use(routes);
 app.use(store);
 app.mount('#app');
---
+++ src/modules/core/components/AppLink.vue
@@ -0,0 +1,32 @@
+<!--
+  AppLink.vue
+  From the excellent article here :   https://vueschool.io/lessons/extending-router-link-for-external-urls
+
+  Sep 28 2019   Initial
+
+-->
+
+<template>
+
+  <a v-if="isExternal" class="external-link" :href="to" target="_blank" rel="noopener"><slot></slot></a>
+
+  <router-link v-else class="internal-link" v-bind="$props" ><slot></slot></router-link>
+
+</template>
+
+<script>
+
+import { RouterLink } from 'vue-router';
+
+export default {
+  props: {
+    ...RouterLink.props
+  },
+
+  computed: {
+    isExternal() {
+      return typeof this.to === 'string' && this.to.startsWith('http');
+    }
+  }
+}
+</script>
